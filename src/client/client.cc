cfe66687ac567e33feb09a4fbc4478871cfa62e1
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2012-12-02 14:19:44 +0200

    make client draw stuff again

--- src/client/client.cc
+++ src/client/client.cc
@@ -1,4 +1,11 @@
+#include <SDL.h>
+#include <iostream>
+
 #include "common/config.h"
+#include "client/view/window.h"
+#include "client/controller/input/input_manager.h"
+#include "client/controller/screen_stack.h"
+#include "client/controller/main_menu.h"
 
 #include "client/client.h"
 
@@ -11,11 +18,29 @@ Client::Client(unsigned ticks_per_sec, int& quit):
 void Client::keepalive()
 {
   m_client_connection.initiate_connection();
-  while (!m_quit)
+  open_window();
+
+  InputManager input_manager;
+  Renderer renderer;
+
+  g_screen_stack.push(&g_main_menu);
+
+  while(!g_screen_stack.empty())
   {
     m_tick_timer.start_tick();
     m_client_connection.tick();
+    Screen* screen = g_screen_stack.back();
+    if (g_screen_stack.check_dirty())
+      screen->Activate();
+
+    screen->Update(input_manager, 0.05f);
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    screen->Draw(renderer);
+    SDL_GL_SwapBuffers();
     m_tick_timer.end_tick();
   }
+
+  SDL_Quit();
 }
 
