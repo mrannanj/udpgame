a3d253f786f88af41cc2f4af94aede62363b2145
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2014-03-13 08:00:47 +0200

    separate intermediate directories for vsprojects

--- vsprojects/script/post_build.ps1
+++ vsprojects/script/post_build.ps1
@@ -1,7 +1,7 @@
-mkdir udpgame -Force
+mkdir udpgame -Force > $null
 copy x64\Release\client.exe udpgame\
 copy x64\Release\server.exe udpgame\
-mkdir udpgame\resources -Force
+mkdir udpgame\resources -Force > $null
 xcopy ..\resources\* udpgame\resources\ /s /e
 copy packages\zlib.redist.1.2.8.6\build\native\bin\v110\x64\Release\dynamic\cdecl\zlib.dll udpgame\
 copy packages\sfml.redist.2.1.0.0\build\native\bin\x64\v110\Release\Desktop\sfml-system-2.dll udpgame\
@@ -14,4 +14,4 @@ copy packages\libjpeg.redist.9.0.1.3\build\native\bin\v110\x64\Release\dynamic\c
 copy packages\libjpeg.redist.9.0.1.3\build\native\bin\v110\x64\Release\dynamic\cdecl\jpeg.dll udpgame\libjpeg.dll
 copy lib\x64_dll\msvcr110.dll udpgame\
 copy lib\x64_dll\msvcp110.dll udpgame\
-tools\7za.exe a -tzip ../udpgame.zip udpgame
+tools\7za.exe a -tzip ..\udpgame.zip udpgame
