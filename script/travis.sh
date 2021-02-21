d90a70f7d30b1e54bc6a47abb1831e2f83a4758a
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2021-02-21 18:16:15 +0200

    script: Install libfreetype-dev in travis.sh

--- script/travis.sh
+++ script/travis.sh
@@ -4,4 +4,4 @@ set -x
 
 sudo apt-get update -qq
 sudo apt-get install -qq libglew-dev libsdl-image1.2-dev libglm-dev \
-  libprotobuf-dev protobuf-compiler
+  libprotobuf-dev protobuf-compiler libfreetype-dev
