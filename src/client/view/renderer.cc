2210c04648537ff0163295d76fe0263190e8387c
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2021-02-24 16:38:26 +0200

    client/view: Add FtRenderer to Renderer

--- src/client/view/renderer.cc
+++ src/client/view/renderer.cc
@@ -5,7 +5,8 @@ Renderer::Renderer():
   texture_manager(resourceLocator),
   text_renderer(resourceLocator, texture_manager[Texture::TEXTURE_FONT]),
   cube_renderer(resourceLocator),
-  quad_renderer(resourceLocator)
+  quad_renderer(resourceLocator),
+  ft_renderer(resourceLocator)
 {
 }
 
--- src/client/view/renderer.h
+++ src/client/view/renderer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "common/resource_locator.h"
+#include "client/view/ft_renderer.h"
 #include "client/view/text_renderer.h"
 #include "client/view/cube_renderer.h"
 #include "client/view/quad_renderer.h"
@@ -15,4 +16,5 @@ public:
   TextRenderer text_renderer;
   CubeRenderer cube_renderer;
   QuadRenderer quad_renderer;
+  FtRenderer ft_renderer;
 };
